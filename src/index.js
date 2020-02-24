2285b95747ec5de8b44474ca588fbcb6ff77d75d
vlavirus <androando777@gmail.com>
2020-02-24 19:40:22 +0300

    feat: add algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let math_n = Math.abs(n);
+    let str_n = String( math_n );
+    let reverse_n = "";
+
+    for ( let i = str_n.length - 1; i >= 0; i-- ) {
+        reverse_n += str_n[i];
+    }
+    
+  return reverse_n;
 }
